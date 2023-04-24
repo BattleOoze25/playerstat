40f4b9972a64c5586c5c9134afdb02c2fca377c1
Navaratna Agarwal <jishuagarwala@gmail.com>
2023-04-24 23:52:49 +0530

    second

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^0yhk-)tm@ksu_$t0jft+sx94hl#turt8y0u_(o1s(ad=6haht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
